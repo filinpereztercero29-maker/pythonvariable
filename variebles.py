d2fbd45639377345d99e23aa90f0d220ad821f30
Agustin <filinpereztercero29@gmail.com>
2025-09-08 16:04:06 -0400

    Initial commit

---
+++ variebles.py
@@ -0,0 +1,17 @@
+mult_x =
+mult_y =
+print(mult_x * mult_y)
+
+div_x =
+div_y =
+print(div_x / div_y)
+print(div_x // div_y)
+print(div_x % div_y)
+
+add_x =
+add_y =
+print(add_x + add_y)
+
+sub_x =
+sub_y =
+print(sub_x - sub_y)
